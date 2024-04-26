fcdc1eb20f433737f3279a2e8c1f2a2203fb353a
Mouni413 <129064482+Mouni413@users.noreply.github.com>
2024-04-26 20:04:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -30,7 +30,8 @@ const options = {
     },
     severs: [
       {
-        url: "http://localhost:8080",
+        // url: "http://localhost:8080",
+        url: "https://nodejs-job-portal-413.onrender.com",
       },
     ],
   },
